879b94c83de7e7034cc24d92698418b4f027d8be
Tomas Escobar <tomasj@unicauca.edu.co>
2020-10-02 12:52:50 -0500

    Se realizaron las migraciones

--- Backend/domi_foods/domi_foods/settings.py
+++ Backend/domi_foods/domi_foods/settings.py
@@ -97,14 +97,25 @@ WSGI_APPLICATION = 'domi_foods.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bd_domi_foods',
+        'USER': 'postgres',
+        'PASSWORD': 'postgresql',
+        'HOST': '127.0.0.1',
+        'DATABASE_PORT':5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
