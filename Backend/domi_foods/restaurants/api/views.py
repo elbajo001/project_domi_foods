1245e4557900b9fa673aeee888292b3017a6ff13
elbajo001 <elbajo002@hotmail.com>
2020-10-12 00:43:33 -0500

    adición tres (api) 1) Listar restaurantes asociados a un admin. 2) Listar categorías asociadas a un restaurante asociado un admin. 3) Listar productos asociados a una categoría asociada a un restaurante asociado a un admin.

--- Backend/domi_foods/restaurants/api/views.py
+++ Backend/domi_foods/restaurants/api/views.py
@@ -18,7 +18,6 @@ class ListProductsByRestaurantByCategory(generics.ListAPIView):
                                        [var_rest, var_cat])
         return queryset
 
-
 # listar los productos de un restaurante <rest>
 class ListProductsByRestaurant(generics.ListAPIView):
     serializer_class = ProductSerializer
@@ -43,6 +42,50 @@ class ListCategoryByRestaurant(generics.ListAPIView):
         queryset = Category.objects.raw('SELECT * FROM restaurants_category '
                                         'INNER JOIN restaurants_category_restaurant ON restaurants_category.id = restaurants_category_restaurant.category_id '
                                         'WHERE restaurants_category_restaurant.restaurant_id = %s', [var_rest])
+        return queryset
+
+# listar los restaurantes asociados a un admin <admin>
+class ListRestaurantsByAdmin(generics.ListAPIView):
+    serializer_class = RestaurantSerializer
+
+    def get_queryset(self):
+        var_admin = self.kwargs['admin']
+        print(self.kwargs)
+        queryset = Restaurant.objects.raw('SELECT * FROM restaurants_restaurant '
+                                       'WHERE restaurants_restaurant.id_admin_id = %s',
+                                       [var_admin])
+        return queryset
+
+# listar las categorías de un restaurante <rest> asociado a un admin <admin>
+class ListCategoriesByRestaurantByAdmin(generics.ListAPIView):
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        var_rest = self.kwargs['rest']
+        var_admin = self.kwargs['admin']
+        print(self.kwargs)
+        queryset = Category.objects.raw('SELECT * FROM restaurants_category '
+                                       'INNER JOIN restaurants_category_restaurant ON restaurants_category.id = restaurants_category_restaurant.category_id '
+                                       'INNER JOIN restaurants_restaurant ON restaurants_restaurant.id = restaurants_category_restaurant.restaurant_id '
+                                       'WHERE restaurants_restaurant.id = %s AND restaurants_restaurant.id_admin_id = %s',
+                                       [var_rest, var_admin])
+        return queryset
+
+# listar los productos asociadosa a una categoría <cat> de un restaurante <rest> asociado a un admin <admin>
+class ListProductsByCategoryByRestaurantByAdmin(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        var_cat = self.kwargs['cat']
+        var_rest = self.kwargs['rest']
+        var_admin = self.kwargs['admin']
+        print(self.kwargs)
+        queryset = Product.objects.raw('SELECT * FROM restaurants_product '
+                                       'INNER JOIN restaurants_category ON restaurants_category.id = restaurants_product.category_id '
+                                       'INNER JOIN restaurants_category_restaurant ON restaurants_category.id = restaurants_category_restaurant.category_id '
+                                       'INNER JOIN restaurants_restaurant ON restaurants_restaurant.id = restaurants_category_restaurant.restaurant_id '
+                                       'WHERE restaurants_restaurant.id = %s AND restaurants_restaurant.id_admin_id = %s AND restaurants_category.id = %s',
+                                       [var_rest, var_admin, var_cat])
         return queryset
 
 
