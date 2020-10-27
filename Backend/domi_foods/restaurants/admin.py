8d21611d02940252cd0cb7dd762932fc3dac4912
elbajo001 <elbajo002@hotmail.com>
2020-10-26 21:04:43 -0500

    cambio modelo bd nuevo

--- Backend/domi_foods/restaurants/admin.py
+++ Backend/domi_foods/restaurants/admin.py
@@ -5,15 +5,15 @@ from .models import *
 # Register your models here.
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ['id_admin', 'nit', 'name', 'address_location', 'phone_num', 'web_page', 'hours', 'image',
-                    'date_creation', 'state_delete', 'state_disponibility']
+    list_display = ['id_admin', 'nit', 'name', 'address_location', 'phone_num', 'web_page', 'hours_start', 'hours_end', 'image',
+                    'state', 'created_at', 'updated_at']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'description', 'image', 'date_creation', 'state_delete', 'state_disponibility']
+    list_display = ['name', 'price', 'description', 'image', 'state', 'created_at', 'updated_at']
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'date_creation', 'state_delete']
+    list_display = ['name', 'description', 'image', 'state', 'created_at', 'updated_at']
