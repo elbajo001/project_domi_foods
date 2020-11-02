816e143c7e999e7bdfdf1e2f20263dd3826ec401
Tomas Escobar <tomasj@unicauca.edu.co>
2020-11-02 10:13:54 -0500

    Se creo la base para la REST API para listar las ordenes de un restaurante, pasando la id del restaurante REST API = V1.0.

--- Backend/domi_foods/shopping_cars/admin.py
+++ Backend/domi_foods/shopping_cars/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-
+"""
 # Register your models here.
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -25,3 +25,4 @@ class ShoppingCartAdmin(admin.ModelAdmin):
 class BillAdmin(admin.ModelAdmin):
     list_display = ['order',
                     'num_bill', 'total_to_pay', 'created_at', 'updated_at']
+"""
--- Backend/domi_foods/shopping_cars/api/serializers.py
+++ Backend/domi_foods/shopping_cars/api/serializers.py
@@ -1,46 +1,8 @@
 from abc import ABC
 from rest_framework import serializers
-from accounts.models import UserRestaurant
-from django.contrib.auth.models import User
+from shopping_cars.models import Order
 
-
-class ChangePasswordSerializer(serializers.Serializer):
-    model = User
-    """
-    Serializer for password change endpoint.
-    """
-    old_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
-
-
-class UserRestaurantSerializer(serializers.ModelSerializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UserRestaurant
+        model = Order
         fields = '__all__'
-
-
-# User Serializer
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username')
-
-
-# Register Serializer
-class RegisterUserSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password', 'password2')
-        extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        password = self.validated_data['password']
-        username = self.validated_data['username']
-        email = self.validated_data['email']
-        password2 = self.validated_data['password2']
-        if password != password2:
-            raise serializers.ValidationError({'answer': 'las contraseñas no coinciden.'})
-        user = User.objects.create_user(username, email, validated_data['password'])
-        return user
