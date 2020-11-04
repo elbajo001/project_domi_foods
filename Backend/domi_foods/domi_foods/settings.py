719963aaf6b587c379d2fa686bc8659856268154
elbajo001 <elbajo002@hotmail.com>
2020-11-03 20:54:42 -0500

    merge master

--- Backend/domi_foods/domi_foods/settings.py
+++ Backend/domi_foods/domi_foods/settings.py
@@ -99,15 +99,15 @@ WSGI_APPLICATION = 'domi_foods.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -118,7 +118,7 @@ DATABASES = {
         'DATABASE_PORT':5432
     }
 }
-
+ """
 
 
 # Password validation
