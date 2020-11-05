765a52ede08d9602e2d5637deb74acc47baf2f2c
Tomas Escobar <tomasj@unicauca.edu.co>
2020-11-05 14:42:09 -0500

    Se coloco la carpeta del venv

--- Backend/domi_foods/domi_foods/settings.py
+++ Backend/domi_foods/domi_foods/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'domi_foods.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -107,18 +107,18 @@ DATABASES = {
     }
 }
 
-""" 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bd_domifoods',
+        'NAME': 'proyecto1',
         'USER': 'postgres',
         'PASSWORD': 'postgresql',
         'HOST': '127.0.0.1',
         'DATABASE_PORT':5432
     }
 }
- """
+
 
 
 # Password validation
