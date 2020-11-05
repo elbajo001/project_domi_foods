24b5d6a2cb0dbef917ff1d50ec8711d6c9e3363f
Tomas Escobar <tomasj@unicauca.edu.co>
2020-11-05 09:45:51 -0500

    Limpieza de Cache

--- Backend/domi_foods/domi_foods/settings.py
+++ Backend/domi_foods/domi_foods/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'domi_foods.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -111,14 +111,14 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bd_domifoods',
+        'NAME': 'unicauca',
         'USER': 'postgres',
         'PASSWORD': 'postgresql',
         'HOST': '127.0.0.1',
         'DATABASE_PORT':5432
     }
 }
- """
+
 
 
 # Password validation
